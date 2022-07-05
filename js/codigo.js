2c3eb77f76ae02fc44bf0b62fb18c9b90a51ecee
esteban <teba.gonzalez.eg@gmail.com>
2022-07-05 02:21:35 -0400

    avance clase 4 creando elementos con js

--- js/codigo.js
+++ js/codigo.js
@@ -7,3 +7,21 @@ class Item  {
         this.crearDiv(nuevaTarea)
     }
 }
+
+crearDiv(nuevaTarea){
+    const inputItem = document.createElement('input');
+    inputItem.classList.add('item-input');
+    inputItem.disabled = true;
+    inputItem.value = nuevaTarea;
+}
+
+const divTarea = document.createElement('div');
+divTarea.classList.add('item');
+
+const btnEditar = document.createElement('button');
+btnEditar.classList.add('btn-editar');
+btnEditar.innerHTML ='<i class="fa-solid fa-pen-to-square"></i>';
+
+const btnRemover = document.createElement('button');
+btnRemover.classList.add('btn-remover');
+btnRemover.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>'
